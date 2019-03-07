2abfda9cd1a396b3c880b29e0f254b5beb9fc8f8
Dana Vergilyush <dvergilyush@gmail.com>
2019-03-08 02:06:31 +0300

    bugs fixed

--- homeWork_4/js/main.js
+++ homeWork_4/js/main.js
@@ -1,48 +1,38 @@
 window.addEventListener('DOMContentLoaded', function () {
 
-	const slides = document.getElementsByClassName('jsSliderItem');
-	const dotsWrap = document.querySelector('.jsSliderDots');
-	const dots = document.querySelectorAll('.jsSliderDot');
-	let slideIndex = 1;
+	slider();
 
-	showSlides(slideIndex);
+	function slider() {
 
-	// функция показа текущего слайда
-	function showSlides(n) {
-		if (n > slides.length) {
-				slideIndex = 1;
+		const slides = document.getElementsByClassName('jsSliderItem');
+		const dotsWrap = document.querySelector('.jsSliderDots');
+		const dots = document.querySelectorAll('.jsSliderDot');
+
+		let forEach = [].forEach;
+
+		showSlide(0);
+
+		function showSlide(n) {
+
+			forEach.call(slides, slide => {
+				slide.classList.remove('main__slider-item_active');
+			});
+			forEach.call(dots, dot => {
+				dot.classList.remove('main__slider-dot_active');
+			});
+
+			slides[n].classList.add('main__slider-item_active');
+			dots[n].classList.add('main__slider-dot_active');
 		};
-		if (n < 1) {
-			slideIndex = slides.length;
-		};
-		for (let i = 0; i < slides.length; i++) {
-			slides[i].style.display = 'none';
-		};
-		for (let i = 0; i < dots.length; i++) {
-			dots[i].classList.remove('active');
-		};
-		slides[slideIndex - 1].style.display = '';
-		dots[slideIndex - 1].classList.add('active');
-	}
 
-	// функция для получения текущего слайда
-	function currentSlide(n) {
-		showSlides(slideIndex = n);
-
-	}
-
-/*	let autoShowNextSlide = setTimeout(autoPlusSlides, 6000);
-	function autoPlusSlides () {
-		showSlides(slideIndex += 1);
-		autoShowNextSlide = setTimeout(autoPlusSlides, 6000);
-	}*/
-
-	dotsWrap.addEventListener('click', event => {
-		for (let i = 1; i <= dots.length; i++) {
-				if (event.target.classList.contains('jsSliderDot') && event.target == dots[i-1]) {
-					currentSlide(i);
+		dotsWrap.addEventListener('click', event => {
+			forEach.call(dots, (dot, i) => {
+				if (event.target == dot) {
+					showSlide(i);
 				}
-		}
-	});
+			});
+		});
+
+	};
 
 })
